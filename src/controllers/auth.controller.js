29b88508c64b95efec84cd3b0fe117e8f09fbaf6
Sonu <sonuroy2029@gmail.com>
2026-02-12 13:08:59 +0530

    feat: add password hashing

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import { User } from "../models/user.model.js";
 
 export const signup = async (req, res) => {
@@ -5,7 +6,7 @@ export const signup = async (req, res) => {
 
   try {
     if (!fullName || !username || !password) {
-      return res.status(400).json({message: 'All fields are required' });
+      return res.status(400).json({ message: 'All fields are required' });
     }
 
     // Check if username is greater than 3 characters
@@ -27,27 +28,32 @@ export const signup = async (req, res) => {
     if (password.length < 8) {
       return res.status(400).json({ message: 'Password must be at least 8 characters' });
     }
+
+    // Encrypt password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword
+    });
+
+    if (newUser) {
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username
+      });
+
+    } else {
+      res.status(400).json({ message: 'Invalid user data' });
+    }
+
   } catch (error) {
     console.log('Error in signup controller', error);
     res.status(500).json({ message: 'Internal server error' });
-  }
-
-  const newUser = new User({
-    fullName,
-    username,
-    password
-  });
-
-  if (newUser) {
-    await newUser.save();
-
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      password: newUser.password
-    });
-  } else {
-    res.status(400).json({ message: 'Invalid user data' });
   }
 }
