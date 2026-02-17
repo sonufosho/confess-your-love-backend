20841f401ca9a5757de213f2022a8868566fb930
Sonu <sonuroy2029@gmail.com>
2026-02-17 14:30:53 +0530

    refactor: update profile schema

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -14,20 +14,24 @@ const profileSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  relationshipType: {
+    type: String,
+    required: true
+  },
   firstHobby: {
     type: String,
-    enum: ['I love cooking', 'I love cats', 'I love dogs', 'I love kids', 'I have a soft heart', 'I have muscules', 'I can drive'],
     required: true
   },
   secondHobby: {
     type: String,
-    enum: ['I can do the dishes', 'I can carry you in games', 'I can give you hugs', 'I can give you kisses', 'I can take care of you', 'I can give you flowers', 'I can lift you up', 'I can take you to long drives', 'I can take you to coffee dates', 'I can take you to dinner dates', 'I can take you to shopping'],
     required: true
   },
   thirdHobby: {
     type: String,
-    enum: ['I would love to cook with you', 'I would love to go with you', 'I would love to hold your hands', 'I would love to workout with you'],
     required: true
+  },
+  message: {
+    type: String
   }
 
 }, { timestamps: true }); // createdAt & updatedAt
